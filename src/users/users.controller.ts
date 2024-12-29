8615eb6b6e0b8dfa9c2f1625c9cdbdd92edee8ad
chienpv <phamvanbk11@gmail.com>
2024-12-29 10:20:25 +0700

    connect mongo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,6 +15,7 @@ import {
 import { Request, Response } from 'express';
 import { Params } from 'src/interface/params';
 import { UsersService } from './users.service';
+import { dtoUser } from 'src/DTO/dto.user';
 
 @Controller('users')
 export class UsersController {
@@ -32,17 +33,50 @@ export class UsersController {
     }
   }
   @Get('detail/:id')
-  details(@Param() params: Params) {
-    const id = params.id;
-
-    const user = this.userService.detail(id);
-    return user;
+  async details(@Param() params: Params, @Res() res: Response) {
+    const id = String(params.id);
+    try {
+      const user = await this.userService.detail(id);
+      res
+        .status(200)
+        .json({ message: 'Lấy dữ liệu thành công!!', datas: user });
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
   }
 
-  @Delete('create')
-  create(@Req() request: Request, @Body() body: any) {
-    console.log(body);
-    return { id: 'abc', name: 'xyz' };
+  @Delete('/delete/:id')
+  async delete(@Res() res: Response, @Req() request: Request) {
+    const id = request.params.id;
+    try {
+      const data = await this.userService.deleteUser(id);
+      res.status(200).json({ message: 'Xoa thanh cong!!', datas: data });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+  @Post('create')
+  async create(@Res() res: Response, @Body() body: dtoUser) {
+    try {
+      const data = await this.userService.createUser(body);
+      res.status(200).json({ message: 'Tao moi thanh cong!!', datas: data });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+  @Put('update/:id')
+  async update(
+    @Param() params: Params,
+    @Res() res: Response,
+    @Body() body: dtoUser,
+  ) {
+    const id = String(params.id);
+    try {
+      const data = await this.userService.updateUser(body, id);
+      res.status(200).json({ message: 'Update thanh cong!!', datas: data });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
 }
 // get
